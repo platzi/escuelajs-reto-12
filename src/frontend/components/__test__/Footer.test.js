77ddb90acf84cfdb5be598d5d8caf5c95d4bce8a
cristubuntu <cristhiam.tovar@mail.escuelaing.edu.co>
2019-11-18 22:01:50 -0500

    test fail because .styl

--- src/frontend/components/__test__/Footer.test.js
+++ src/frontend/components/__test__/Footer.test.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import { create } from 'react-test-renderer';
-import { render, shallow, configure } from 'enzyme';
+import { render, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import Footer from '../Footer';
 
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
-describe('Footer testing', ()=>{
+describe('Footer testing', () => {
   test('Match snapshot', () => {
-    const footer =create(<Footer />);
+    const footer = create(<Footer />);
     expect(footer.toJSON()).toMatchSnapshot();
   });
 
-  test('footer has class .footer', () => {
-    const footer = shallow(<Footer />);
-    const footerElem = footer.find('footer');
-    expect(footerElem.hasClass('footer')).toBe(true);
+  test('footer has at least 1 division tag', () => {
+    const footer = render(<Footer />);
+    expect(footer.find('p')).toHaveLength(1);
   });
 
-  test('footer has at least 1 anchor tags', () => {
+  test('footer has at least 1 anchor tag', () => {
     const footer = render(<Footer />);
-    expect(footer.find('p')).toHaveLength(1)
+    expect(footer.find('p')).toHaveLength(1);
   });
 });
